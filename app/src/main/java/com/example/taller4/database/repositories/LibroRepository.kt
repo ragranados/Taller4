4a50149239e9e0fb74f5288012a7884e64103580
Carlos Ruiz <00095417@uca.edu.sv>
2019-05-23 08:14:32 -0600

    agregando parte del repository

--- app/src/main/java/com/example/taller4/database/repositories/LibroRepository.kt
+++ app/src/main/java/com/example/taller4/database/repositories/LibroRepository.kt
@@ -1,5 +1,6 @@
 package com.example.taller4.database.repositories
 
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import com.example.taller4.DAOs.AutorDAO
 import com.example.taller4.DAOs.EditorialDAO
@@ -27,6 +28,44 @@ class LibroRepository(private val libroDao:LibroDAO,
     //obtener tags
     fun getAllTags(): LiveData<List<Tags>> = tagsDao.getallTags()
 
+    //borrar libro
+    fun deleteLibro(libro:Libro) = libroDao.deleteLibro(libro)
 
+    //borrar autor
+    fun deleteAutor(autor:Autor) = autorDao.deleteAutor(autor)
+
+    //borrar editorial
+    fun deleteEditorial(editorial:Editorial) = editorialDao.deleteEditorial(editorial)
+
+    //borrar tag
+    fun deleteTag(tag:Tags) = tagsDao.deleteTag(tag)
+
+    //insertar libro
+    @WorkerThread
+    suspend fun insertLibro(libro:Libro) = libroDao.insertLibro(libro)
+
+    //insertar autor
+    @WorkerThread
+    suspend fun inertAutor(autor:Autor) = autorDao.insertAutor(autor)
+
+    //insertar editorial
+    @WorkerThread
+    suspend fun insertEditorial(editorial:Editorial) = editorialDao.insertEditorial(editorial)
+
+    //insertar tag
+    @WorkerThread
+    suspend fun insertTag(tag:Tags) = tagsDao.insertTag(tag)
+
+    //actualizar libro
+    fun updateLibro(libro:Libro) = libroDao.updateLibro(libro)
+
+    //actualizar autor
+    fun updateAutor(autor:Autor) = autorDao.updateAutor(autor)
+
+    //actualizar editorial
+    fun updateEditorial(editorial:Editorial) = editorialDao.update(editorial)
+
+    //actualizar tags
+    fun updateTags(tag:Tags) = tagsDao.update(tag)
 
 }
