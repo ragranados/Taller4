8df5346c2d1f7238528461d44c5c32ad40ca80f8
Carlos Ruiz <00095417@uca.edu.sv>
2019-05-23 17:17:26 -0600

    agregando parte del repository

--- app/src/main/java/com/example/taller4/database/repositories/LibroRepository.kt
+++ app/src/main/java/com/example/taller4/database/repositories/LibroRepository.kt
@@ -16,56 +16,82 @@ class LibroRepository(private val libroDao:LibroDAO,
                       private val editorialDao:EditorialDAO,
                       private val tagsDao: TagsDAO) {
 
+    //-----------------------------Libros---------------------------------------------
+
+    //obtener libro
+    fun findLibroByName(name: String): List<Libro> = libroDao.findLibroByName(name)
+
     //obtener libros
     fun getAllLibros(): LiveData<List<Libro>> = libroDao.getAllLibros()
 
-    //obtener autores
-    fun getAllAutores(): LiveData<List<Autor>> = autorDao.getallAutores()
-
-    //obtener editorial
-    fun getAllEditoriales(): LiveData<List<Editorial>> = editorialDao.getallEditorial()
-
-    //obtener tags
-    fun getAllTags(): LiveData<List<Tags>> = tagsDao.getallTags()
-
     //borrar libro
     fun deleteLibro(libro:Libro) = libroDao.deleteLibro(libro)
-
-    //borrar autor
-    fun deleteAutor(autor:Autor) = autorDao.deleteAutor(autor)
-
-    //borrar editorial
-    fun deleteEditorial(editorial:Editorial) = editorialDao.deleteEditorial(editorial)
-
-    //borrar tag
-    fun deleteTag(tag:Tags) = tagsDao.deleteTag(tag)
 
     //insertar libro
     @WorkerThread
     suspend fun insertLibro(libro:Libro) = libroDao.insertLibro(libro)
 
+    //actualizar libro
+    fun updateLibro(libro:Libro) = libroDao.updateLibro(libro)
+
+    //--------------------------------------------------------------------------------
+
+    //-------------------------------Autor--------------------------------------------
+
+    //obtener autor
+    fun getAutorByLibro(libroId: Int): List<Autor> = autorDao.getAutorByLibro(libroId)
+
+    //obtener autores
+    fun getAllAutores(): LiveData<List<Autor>> = autorDao.getallAutores()
+
+    //borrar autor
+    fun deleteAutor(autor:Autor) = autorDao.deleteAutor(autor)
+
     //insertar autor
     @WorkerThread
     suspend fun inertAutor(autor:Autor) = autorDao.insertAutor(autor)
+
+    //actualizar autor
+    fun updateAutor(autor:Autor) = autorDao.updateAutor(autor)
+
+    //--------------------------------------------------------------------------------
+    //-------------------------------Editorial----------------------------------------
+
+    //obtener editorial
+    fun getEditorialByLibro(libroId: Int): List<Editorial> = editorialDao.getEditorialByLibro(libroId)
+
+    //obtener editoriales
+    fun getAllEditoriales(): LiveData<List<Editorial>> = editorialDao.getallEditorial()
+
+    //borrar editorial
+    fun deleteEditorial(editorial:Editorial) = editorialDao.deleteEditorial(editorial)
 
     //insertar editorial
     @WorkerThread
     suspend fun insertEditorial(editorial:Editorial) = editorialDao.insertEditorial(editorial)
 
+    //actualizar editorial
+    fun updateEditorial(editorial:Editorial) = editorialDao.update(editorial)
+
+    //--------------------------------------------------------------------------------
+    //---------------------------------Tags-------------------------------------------
+
+   //obtener tag
+   fun getTagsByLibro(libroId: Int): List<Tags> = tagsDao.getTagsByLibro(libroId)
+
+    //obtener tags
+    fun getallTags(): LiveData<List<Tags>> = tagsDao.getallTags()
+
+    //borrar tag
+    fun deleteTag(tag:Tags) = tagsDao.deleteTag(tag)
+
     //insertar tag
     @WorkerThread
     suspend fun insertTag(tag:Tags) = tagsDao.insertTag(tag)
 
-    //actualizar libro
-    fun updateLibro(libro:Libro) = libroDao.updateLibro(libro)
-
-    //actualizar autor
-    fun updateAutor(autor:Autor) = autorDao.updateAutor(autor)
-
-    //actualizar editorial
-    fun updateEditorial(editorial:Editorial) = editorialDao.update(editorial)
-
     //actualizar tags
     fun updateTags(tag:Tags) = tagsDao.update(tag)
+
+    //------------------------------------------------------------------------------
 
 }
